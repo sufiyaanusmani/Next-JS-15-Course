532975a02fd0524668eccf2bd6f8648fa6cadda5
Sufiyaan Usmani <usmanisufiyaan@gmail.com>
2024-11-12 23:27:25 +0500

    Set up authentication

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,47 @@
+import Link from 'next/link'
+import React from 'react'
+import Image from 'next/image'
+import { auth, signOut, signIn } from '@/auth'
+
+const Navbar = async () => {
+    const session = await auth();
+  return (
+    <>
+        <header className='px-5 py-3 bg-white shadow-sm font-work-sans'>
+            <nav className='flex justify-between items-center'>
+                <Link href="/">
+                    <Image src="/logo.png" alt="Logo" width={64} height={30}></Image>
+                </Link>
+
+                <div className="flex item-center gap-5 text-black">
+                    {session && session?.user ? (
+                        <>
+                            <Link href="/startup/create"><span>Create</span></Link>
+
+                            <form action={async () => {
+                                "use server";
+                                await signOut({ redirectTo: "/"})
+                            }}>
+                                <button type='submit'>Logout</button>
+                            </form>
+
+                            <Link href={`/user/${session?.id}`}><span>{session?.user?.name}</span></Link>
+                        </>
+                    ): (
+                        <>
+                            <form action={async () => {
+                                "use server";
+                                await signIn("github")
+                            }}>
+                                <button type="submit">Login</button>
+                            </form>
+                        </>
+                    )}
+                </div>
+            </nav>
+        </header>
+    </>
+  )
+}
+
+export default Navbar
